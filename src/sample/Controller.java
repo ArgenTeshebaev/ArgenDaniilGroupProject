5ccd8333e077992ed6a003c8143165366d72dd63
Argen <65073960+ArgenTeshebaev@users.noreply.github.com>
2020-12-25 02:15:46 +0600

    Add files via upload

---
+++ src/sample/Controller.java
@@ -0,0 +1,86 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView eda;
+
+    @FXML
+    private ImageView jile;
+
+    @FXML
+    private ImageView gigiena;
+
+    @FXML
+    private ImageView mashina;
+
+    @FXML
+    private ImageView odejda;
+
+    @FXML
+    private ImageView kafe;
+
+    @FXML
+    private ImageView scheta;
+
+    @FXML
+    private ImageView zdorovie;
+
+    @FXML
+    private ImageView taxi;
+
+    @FXML
+    private ImageView razvlecheniya;
+
+    @FXML
+    private ImageView pitomsy;
+
+    @FXML
+    private ImageView svyaz;
+
+    @FXML
+    private ImageView sport;
+
+    @FXML
+    private ImageView transport;
+
+    @FXML
+    private ImageView podarki;
+
+    @FXML
+    private TextField add;
+
+    @FXML
+    private ImageView zarplata;
+
+    @FXML
+    private ImageView sberejeniya;
+
+    @FXML
+    private ImageView depoziti;
+
+    @FXML
+    private Label rashody;
+
+    @FXML
+    private Label schet;
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
